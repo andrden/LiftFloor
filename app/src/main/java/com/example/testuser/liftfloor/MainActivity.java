b40cb6cb8a1853fa4e5cc6ed8e4b687cb98bc028
denny <andrden@gmail.com>
2015-06-08 14:40:29 +0300

    gravity sensor

--- app/src/main/java/com/example/testuser/liftfloor/MainActivity.java
+++ app/src/main/java/com/example/testuser/liftfloor/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.testuser.liftfloor;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SensorEventListener {
+    SensorManager senSensorManager;
+    Sensor senAccelerometer;
+    long tprev=System.currentTimeMillis();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
 
@@ -35,5 +49,39 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            TextView tv = (TextView)findViewById(R.id.xyz);
+            tv.setText(x+ " "+y+" "+z+" dt="+(System.currentTimeMillis()-tprev)+" acc="+sensorEvent.accuracy);
+            tprev = System.currentTimeMillis();
+        }
+    }
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //SensorManager.SENSOR_STATUS_ACCURACY_HIGH=3;
+        TextView tv = (TextView)findViewById(R.id.acc);
+        tv.setText("acc="+accuracy);
     }
 }
