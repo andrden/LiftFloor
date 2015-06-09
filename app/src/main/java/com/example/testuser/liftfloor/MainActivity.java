793bac689527d2dd8f6623001367ff4cd14d6b55
denny <andrden@gmail.com>
2015-06-09 14:28:08 +0300

    TYPE_LINEAR_ACCELERATION doesn't work, so just averaging gravity manually

--- app/src/main/java/com/example/testuser/liftfloor/MainActivity.java
+++ app/src/main/java/com/example/testuser/liftfloor/MainActivity.java
@@ -9,13 +9,42 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity implements SensorEventListener {
+public class MainActivity extends ActionBarActivity {
     SensorManager senSensorManager;
+
     Sensor senAccelerometer;
-    long tprev=System.currentTimeMillis();
+    SensorEventListener accListener;
+    long tprevAcc=System.currentTimeMillis();
+
+    float hmeter = 0;
+    Avg avgGravity = new Avg();
+
+    class Avg{
+        int count=0;
+        double sum=0;
+        void add(float v){
+            sum += v;
+            count++;
+        }
+        double avg(){
+            if( count < 1 ){
+                return 0;
+            }
+            return sum / count;
+        }
+        void reset(){
+            count=0;
+            sum=0;
+        }
+    }
+
+    public void resetAvg(View view) {
+        avgGravity.reset();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +53,31 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
+                // SENSOR_DELAY_NORMAL
+        accListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float x = sensorEvent.values[0];
+                float y = sensorEvent.values[1];
+                float z = sensorEvent.values[2];
+
+                avgGravity.add(z);
+                TextView tvAvg = (TextView)findViewById(R.id.xyzAvg);
+                tvAvg.setText("avg="+avgGravity.avg()+" "+avgGravity.count);
+
+
+                TextView tv = (TextView)findViewById(R.id.xyz);
+                tv.setText(" dt="+(System.currentTimeMillis()-tprevAcc));
+                tprevAcc = System.currentTimeMillis();
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+
+        senSensorManager.registerListener(accListener, senAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
 
@@ -53,35 +105,15 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 
     protected void onPause() {
         super.onPause();
-        senSensorManager.unregisterListener(this);
+        senSensorManager.unregisterListener(accListener);
     }
 
     protected void onResume() {
         super.onResume();
-        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        Sensor mySensor = sensorEvent.sensor;
-
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
-
-            TextView tv = (TextView)findViewById(R.id.xyz);
-            tv.setText(x+ " "+y+" "+z+" dt="+(System.currentTimeMillis()-tprev)+" acc="+sensorEvent.accuracy);
-            tprev = System.currentTimeMillis();
-        }
+        senSensorManager.registerListener(accListener, senAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
 
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        //SensorManager.SENSOR_STATUS_ACCURACY_HIGH=3;
-        //TextView tv = (TextView)findViewById(R.id.acc);
-        //tv.setText("acc="+accuracy);
-    }
+
 }
